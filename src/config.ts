fece3673605ebe731304200a498d311c8a4dc415
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-01-13 22:14:44 -0600

    feat: init

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Home | Curtis Rubeck';
+export const SITE_DESCRIPTION = 'Experienced DevOps/Cloud Engineer in designing, implementing, and managing complex infrastructures. A results-oriented self-starter who loves using tech to solve tricky business problems. I enjoy exploring new ideas and making solutions simple.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
